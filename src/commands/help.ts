1633d5f3fb15ca0cb6978fcfea9fa832156f613f
Nguyễn Duy Tình <tinhnd1902@gmail.com>
2025-03-12 19:08:53 +0700

    fix bug /help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,11 +6,11 @@ export const helpCommand = (ctx: Context) => {
     
 Bạn có thể dùng các lệnh sau:
 
-🚀 \`/start\` - Bắt đầu bot  
-ℹ️ \`/help\` - Xem hướng dẫn sử dụng  
-👤 \`/adduser\` - Thêm người dùng  
-📍 \`/checkin\` - Check-in  
+🚀 \`/start\` \\- Bắt đầu bot  
+ℹ️ \`/help\` \\- Xem hướng dẫn sử dụng  
+👤 \`/adduser\` \\- Thêm người dùng  
+📍 \`/checkin\` \\- Check\\-in  
 
-💡 *Mẹo:* Bạn có thể nhập trực tiếp lệnh để bot thực hiện nhanh chóng!`,
+💡 *Mẹo:* Bạn có thể nhập trực tiếp lệnh để bot thực hiện nhanh chóng\\!`,
   );
 };
